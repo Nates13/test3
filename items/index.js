3723551120be8a70c87b02be5393b3baa375bd1b
Nates13 <76265189+Nates13@users.noreply.github.com>
2020-12-22 14:21:06 -0500

    Update index.js

--- items/index.js
+++ items/index.js
@@ -1,5 +1,5 @@
 export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/Marketplacefoto.png"},
 {"id": "item2", "fname":"item2.md", "specialImage":"/img/FavouriteDay750x500_pdni4p.png"},
-
+{"id": "item3", "fname":"item3.md", "specialImage":"/img/FavouriteDay750x500_pdni4p.png"},
 
 
